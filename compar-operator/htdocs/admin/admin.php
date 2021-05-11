ef69fcba09cc6daf1ec81f707773fe56d2ad981a
Ronan Perrin <77976485+LeRonch@users.noreply.github.com>
2021-05-11 11:59:19 +0200

    Delete compar-operator/htdocs directory

--- compar-operator/htdocs/admin/admin.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-header('Location:loginAdmin.php');
--- compar-operator/htdocs/config/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function loadClass($class_name)
-{
-  require '/shared/httpd/compar-operator/htdocs/class/'. $class_name . '.php'; 
-}
-
-spl_autoload_register('loadClass');
